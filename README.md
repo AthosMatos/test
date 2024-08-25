d68405ee2dc8d9149cc2c22fff039e5b2a171944
Athos Matos <ladiesman217.as@gmail.com>
2024-08-25 10:11:56 -0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple App to test React Components
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@athosws/react-components';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type='action'>
+        TESTE
+      </Button>
+    </div>
+  );
+}
+
+export default App;
